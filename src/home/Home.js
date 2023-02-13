aecaffde014b6a2dd868dba4d2a046b81ae4ab26
DanteLaviolette <dante.laviolette@gmail.com>
2023-02-13 01:03:29 -0500

    Proxy requests to express port

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,9 +1,18 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Home.css';
 
 const SOME_VAR = "Home Page";
 
 function Home() {
+  const [valueFromEndpoint, setValueFromEndpoint] = useState("Loading...");
+  fetch('/health_check').then(res => {
+    if (res.status === 200) {
+      setValueFromEndpoint("Successfully reached backend w/ API call!");
+    } else {
+      setValueFromEndpoint("Failed to reach backend w/ API call :(");
+    }
+  })
   return (
     <div>
       <h1>{SOME_VAR}</h1>
@@ -14,6 +23,7 @@ function Home() {
         <li><Link to="/chat">Chat</Link></li>
         <li><Link to="/queue">Queue</Link></li>
       </ul>
+      <h2>{valueFromEndpoint}</h2>
     </div>
   );
 }
