8d0520bc6d50f93997cb64d488863e8b1820f47b
DanteLaviolette <dante.laviolette@gmail.com>
2023-02-14 03:22:11 -0500

    admin portal unregister user functionality

--- src/agent/dashboard/admin/AdminDashboard.js
+++ src/agent/dashboard/admin/AdminDashboard.js
@@ -26,8 +26,7 @@ function AdminDashboard() {
             </Grid>
             <Grid xs={4}>
                 <Card>
-                    <Typography level='h6'>Unregister Agent</Typography>
-                    <Typography color="neutral" fontSize='sm'>Set an agent to "unregistered" and reset their password.</Typography>
+                    <UnregisterAgentForm />
                 </Card>
             </Grid>
             <Grid xs={6}>
@@ -44,6 +43,59 @@ function AdminDashboard() {
             </Grid>
         </Grid>
     );
+}
+
+function UnregisterAgentForm() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    const [errMsg, setErrMsg] = useState("");
+    const [successMsg, setSuccessMsg] = useState("");
+
+    const unregisterUser = (username) => {
+        setIsLoading(true);
+        setErrMsg("");
+        setSuccessMsg("");
+        setPassword("");
+        axios.post('/admin/unregister_user', {
+            username
+        }).then(res => {
+            setPassword(res.data.password);
+            setSuccessMsg("Unregistered " + username);
+        }).catch(err => {
+            if (err.response.status === 404) {
+                setErrMsg("Username not found");
+            } else {
+                setErrMsg("Something went wrong. Try again later.");
+            }
+        }).finally(() => {
+            setIsLoading(false);
+        })
+    }
+
+    return <div>
+        <Typography level='h6'>Unregister Agent</Typography>
+        <Typography color="neutral" fontSize='sm'>Set an agent to "unregistered" and reset their password.</Typography>
+        <Divider sx={{ mt: '10px', mb: '10px' }} />
+        {isLoading && <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}><CircularProgress /></div>}
+            {!isLoading && <form onSubmit={e => { e.preventDefault(); unregisterUser(username) }}>
+                <FormControl>
+                    <FormLabel>Username</FormLabel>
+                    <Input required type="username" placeholder="Username" value={username} onChange={e => setUsername(e.target.value)} />
+                </FormControl>
+                <FormControl>
+                    <FormLabel>Password</FormLabel>
+                    <Input readonly type="text" placeholder="To be generated" value={password} style={{ color: 'var(--joy-palette-neutral-200)' }}
+                        endDecorator={
+                            <Button color='neutral' onClick={() => navigator.clipboard.writeText(password)}>Copy</Button>
+                        }
+                    />
+                </FormControl>
+                {errMsg !== "" && <Typography color="danger" fontSize="sm" marginTop={1}>{errMsg}</Typography>}
+                {successMsg !== "" && <Typography color="success" fontSize="sm" marginTop={1}>{successMsg}</Typography>}
+                <Button type='submit' sx={{ mt: 1, width: '100%' }}>Unregister Agent</Button>
+            </form>}
+    </div>
 }
 
 function DeleteAgentForm() {
@@ -73,7 +125,7 @@ function DeleteAgentForm() {
 
     return <div>
         <Typography level='h6'>Delete Agent</Typography>
-        <Typography color="neutral" fontSize='sm'>Remove an agents account.</Typography>
+        <Typography color="neutral" fontSize='sm'>Remove an agents account.<br/>You can also remove admin accounts.</Typography>
         <Divider sx={{ mt: '10px', mb: '10px' }} />
         {isLoading && <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}><CircularProgress /></div>}
             {!isLoading && <form onSubmit={e => { e.preventDefault(); deleteUser(username) }}>
@@ -96,13 +148,17 @@ function NewAgentForm() {
 
     const [isLoading, setIsLoading] = useState(false);
     const [errMsg, setErrMsg] = useState("");
+    const [successMsg, setSuccessMsg] = useState("");
 
     const registerUser = (username, firstName, lastName) => {
         setIsLoading(true);
+        setSuccessMsg("");
         setErrMsg("");
+        setPassword("");
         axios.post('/admin/register_temp_user', {
             username, firstName, lastName
         }).then(res => {
+            setSuccessMsg("Registered " + username);
             setPassword(res.data.password);
         }).catch(err => {
             if (err.response.status === 409) {
@@ -144,6 +200,7 @@ function NewAgentForm() {
                     />
                 </FormControl>
                 {errMsg !== "" && <Typography color="danger" fontSize="sm" marginTop={1}>{errMsg}</Typography>}
+                {successMsg !== "" && <Typography color="success" fontSize="sm" marginTop={1}>{successMsg}</Typography>}
                 <Button type='submit' sx={{ mt: 1, width: '100%' }}>Register</Button>
             </form>}
         </div>
